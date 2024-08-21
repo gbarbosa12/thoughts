ba276e155656d0c0a77e3da5edf0831b88f8a1e6
Gabriel Amaral <gabriel2012amaral@gmail.com>
2024-08-20 22:43:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+//  Models
+const Thought = require("./models/Thought");
+const User = require("./models/User");
+
+// Import Routes
+const thoughtsRoutes = require("./routes/thoughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+// Import Controller
+const ThoughtController = require("./controllers/ThoughtsController");
+
+// Template engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// Session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+    },
+  })
+);
+
+// Flash messages
+app.use(flash());
+
+// Public path
+app.use(express.static("public"));
+
+// set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+// Routes
+app.use("/thoughts", thoughtsRoutes);
+app.use("/", authRoutes);
+
+app.get("/", ThoughtController.showThoughts);
+
+conn
+  // .sync({ force: true })
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(console.error()));
---
+++ routes/thoughtsRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const ThoughtsController = require("../controllers/ThoughtsController");
+
+// helpers
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/dashboard", checkAuth, ThoughtsController.dashboard);
+router.get("/", ThoughtsController.showThoughts);
+
+module.exports = router;
